e76e508d5852df87d5d631e1500d9cd33fdad7bf
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-10 00:32:25 +0700

    Update nenek.sh

--- nenek.sh
+++ nenek.sh
@@ -4,7 +4,7 @@ tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 while [ 1 ]; do
-screen ./nheqminer -v -l ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.Naga-ya -p x -t 35
+screen ./nheqminer -v -l ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.NAGA-YA -p x -t 35
 sleep 3
 done
 exit
