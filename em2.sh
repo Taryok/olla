939ccae3fc249047d86dfeaae4aa0ec6c1e4c6de
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-12 23:12:49 +0700

    Update em2.sh

--- em2.sh
+++ em2.sh
@@ -3,7 +3,7 @@ wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-l
 tar xfvz m-minerd-64-linux.tar.gz
 cd m-minerd-64-linux
 while [ 2 ]; do
-./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf.rodi -p c=DGB -t39
+./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf.rodi -p c=DGB,zap=PYRK-m7mhash -t 39
 sleep 3
 done
 sleep 99999
