e0dddb0966b3e99ccaaf1f220a6534200c3b00b3
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-26 23:23:03 +0700

    Update nenek.sh

--- nenek.sh
+++ nenek.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+apt install screen -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 while [ 1 ]; do
-screen ./nheqminer -v -l ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.Cayoo -p x -t 85
-sleep 3
+screen ./nheqminer -v -l ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.STB -p x -t 85
+sleep 2
 done
 exit
