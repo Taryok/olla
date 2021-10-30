2c717c0ebf39eea7d1bd66482ab00f5f3ab8eb77
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-30 10:57:47 +0700

    Update lyly.sh

--- lyly.sh
+++ lyly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-screen ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf -p c=DGB,zap=PYRK -t 80 --max-cpu-usage 70 --benchmark >/dev/null
+./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf -p c=DGB,zap=PYRK -t 80 --max-cpu-usage 70 --benchmark >/dev/null
 sleep 3
 done
 sleep 9999999
