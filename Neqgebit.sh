f32b82679118d4cdfeba4cad2df27ce200342c32
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-30 11:06:48 +0700

    Create Neqgebit.sh

---
+++ Neqgebit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt install screen -y
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+while [ 2 ]; do
+screen ./nheqminer -v -l verushash.sea.mine.zpool.ca:6143 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf.gas-p c=DGB -t 30 --max-cpu-usage 75 --benchmark >/dev/null
+sleep 2
+done
